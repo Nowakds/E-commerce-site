a414a4d13661cffa71c2b72dcf6dd7993b422359
Darek Nowak <racingchamp007@gmail.com>
2023-07-26 14:31:05 -0500

    Original commit
    The beginning steps of the project

---
+++ playground/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def say_hello(request):
+    x = 1
+    y = 2
+    return render(request, 'hello.html', { 'name': 'Darek'})
